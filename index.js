382416c68633d1d493880f724ff2a92df297af87
John Oblenda <joandong2@gmail.com>
2020-01-21 20:27:48 -0500

    MVP and stretch

--- index.js
+++ index.js
@@ -39,8 +39,22 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+    this.name = name,
+    this.age = age,
+    this.stomach = []
+}
+Person.prototype.eat = function(food) {
+    if(this.stomach.length === 10) {
+        return;
+    }
+    this.stomach.push(food);
+}
+Person.prototype.poop = function() {
+    this.stomach = [];
+}
+Person.prototype.toString = function() {
+    return `${this.name}, ${this.age}`;
 }
 
 /*
@@ -57,8 +71,31 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+}
+Car.prototype.fill = function(gallons) {
+    this.tank += gallons;
+}
+Car.prototype.drive = function(distance) {
+    // sample data
+    // distance = 30 miles
+    // milesPerGallon = 21 miles
 
+    const neededFuel = distance/this.milesPerGallon;
+    // 1.4286 gallons
+
+    if(this.tank < neededFuel) {
+        this.odometer = (distance * this.tank) / neededFuel;
+        this.tank = 0;
+        return `I ran out of fuel at ${this.odometer} miles!`;
+    } 
+
+    this.tank -= neededFuel;
+    this.odometer += distance;
 }
 
 /*
@@ -68,8 +105,14 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+const Baby = function(name, age, favoriteToy) {
+    Person.call(this, name, age);
+    this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
 
+Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`;
 }
 
 /* 
